25b0caae6fb5c701c205a7962128df094b20034c
Diyarka <240323@astanait.edu.kz>
2026-07-15 19:21:40 +0500

    blocks added

--- src/main/java/net/diyarnagibaster/huntrio/HunTrio.java
+++ src/main/java/net/diyarnagibaster/huntrio/HunTrio.java
@@ -1,21 +1,12 @@
 package net.diyarnagibaster.huntrio;
 
+import net.diyarnagibaster.huntrio.blocks.ModBlocks;
+import net.diyarnagibaster.huntrio.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
 
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
@@ -25,10 +16,6 @@ import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.registries.DeferredRegister;
 
 @Mod(HunTrio.MODID)
 public class HunTrio {
@@ -41,6 +28,9 @@ public class HunTrio {
 
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
         modEventBus.addListener(this::addCreative);
 
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
@@ -51,7 +41,11 @@ public class HunTrio {
     }
 
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.ALUMINIUM);
+            event.accept(ModItems.ALUMINIUM_INGOT);
+            event.accept(ModBlocks.ALUMINIUM_BLOCK);
+        }
     }
 
     @SubscribeEvent
--- src/main/java/net/diyarnagibaster/huntrio/item/ModItems.java
+++ src/main/java/net/diyarnagibaster/huntrio/item/ModItems.java
@@ -1,5 +1,22 @@
-﻿package net.diyarnagibaster.huntrio.item;
+package net.diyarnagibaster.huntrio.item;
+
+import net.diyarnagibaster.huntrio.HunTrio;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(HunTrio.MODID);
 
+    public static final DeferredItem<Item>
+            ALUMINIUM = ITEMS.register("aluminium", () -> new Item(new Item.Properties())),
+            ALUMINIUM_INGOT = ITEMS.register("aluminium_ingot", () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus){
+
+        ITEMS.register(eventBus);
+
+    }
 }
